ba63f74d715f69bd6c89cd144476c997ac375006
PiotrFe <38947052+PiotrFe@users.noreply.github.com>
2020-10-05 21:05:52 +0200

    lesson

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,6 @@ module.exports = {
   googleClientID:
     '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
   googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  mongoURI: 'mongodb://piotrfer:tv946eXXHArpxNYt@cluster0-shard-00-00.cx9ox.mongodb.net:27017,cluster0-shard-00-01.cx9ox.mongodb.net:27017,cluster0-shard-00-02.cx9ox.mongodb.net:27017/db?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
   cookieKey: '123123123',
 };
