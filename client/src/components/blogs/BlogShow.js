0c5af0e274eee2a8756166e1e10bc69cad09374f
PiotrFe <38947052+PiotrFe@users.noreply.github.com>
2020-10-10 22:57:27 +0200

    lesson

--- client/src/components/blogs/BlogShow.js
+++ client/src/components/blogs/BlogShow.js
@@ -1,3 +1,4 @@
+
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchBlog } from '../../actions';
@@ -5,6 +6,13 @@ import { fetchBlog } from '../../actions';
 class BlogShow extends Component {
   componentDidMount() {
     this.props.fetchBlog(this.props.match.params._id);
+  }
+
+  renderImage() {
+    if (this.props.blog.imageUrl) {
+      return <img src={this.props.blog.imageUrl} />
+
+    }
   }
 
   render() {
@@ -18,6 +26,7 @@ class BlogShow extends Component {
       <div>
         <h3>{title}</h3>
         <p>{content}</p>
+        {this.renderImage()}
       </div>
     );
   }
